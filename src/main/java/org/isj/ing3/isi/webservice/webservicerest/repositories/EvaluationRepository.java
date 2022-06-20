048705f0c8bce3793fa50965725006c32ce55769
nkot20 <etiennenkot1@gmail.com>
2022-06-20 22:05:22 +0000

    Revert "first commit"
    This reverts commit 3571ff0505ba31d221be2cbf10f43b1c2a900358

--- src/main/java/org/isj/ing3/isi/webservice/webservicerest/repositories/EvaluationRepository.java
+++
@@ -1,9 +0,0 @@
-package org.isj.ing3.isi.webservice.webservicerest.repositories;
-
-import org.isj.ing3.isi.webservice.webservicerest.model.entities.Evaluation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
-}
